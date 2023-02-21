315b63a28eeeac400b40da746f83560b00b38315
uma <yadavuma419@gmail.com>
2023-02-21 11:53:43 +0530

    intial commit

---
+++ assets/js/app.js
@@ -0,0 +1,56 @@
+// const startBtn = document.getElementById("start")
+// const captureBtn = document.getElementById("capture")
+// const closeBtn = document.getElementById("close")
+// const player = document.getElementById("player")
+// const canvas = document.getElementById("canvas")
+// const restartBtn = document.getElementById("restart")
+import {
+    startBtn,
+    captureBtn,
+    closeBtn,
+    player,
+    switchBtn,
+    canvas,
+    restartBtn,
+    mode,
+} from "./constants.js"
+import { openCamera, closeCamera } from "./camera.js"
+
+
+
+
+switchBtn.addEventListener("click", () => {
+    mode = mode === "environment" ? "user" : "environment"
+    player.classList.add("d-none")
+    const tracks = player.srcObject.getVideoTracks()
+    for (let i = 0; i < tracks.length; i++) {
+        tracks[i].stop()
+    }
+
+    openCamera()
+
+})
+
+startBtn.addEventListener("click", () => {
+    openCamera()
+
+})
+captureBtn.addEventListener("click", () => {
+    console.log("capture button clicked")
+
+    const preview = canvas.getContext("2d")
+    preview.drawImage(player, 0, 0, canvas.height, canvas.width)
+    canvas.classList.remove("d-none")
+    player.classList.add("d-none")
+    captureBtn.classList.add("d-none")
+    restart.classList.remove("d-none")
+
+})
+restartBtn.addEventListener("click", () => {
+    closeCamera()
+    openCamera()
+})
+closeBtn.addEventListener("click", () => {
+    closeCamera()
+
+})
---
+++ assets/js/constants.js
@@ -0,0 +1,8 @@
+export const startBtn = document.getElementById("start")
+export const captureBtn = document.getElementById("capture")
+export const closeBtn = document.getElementById("close")
+export const player = document.getElementById("player")
+export const canvas = document.getElementById("canvas")
+export const restartBtn = document.getElementById("restart")
+export const switchBtn = document.getElementById("switchCamera")
+export let mode = "user"
